fa7845df825d689840bed19bcc5a7c60e1117a4b
Kushagra Mishra <kushagramishra@Kushagras-MacBook-Air.local>
2024-04-09 18:53:37 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,19 @@
+            const heading = React.createElement(
+                "h1", 
+                {id : "heading"}, 
+                "Hello Mr.!"
+                );
+            
+            
+
+            const parent = React.createElement("div",{id: "parent"},
+                [React.createElement("div",{id: "child1"},
+                    [React.createElement("h1",{},"My Name is Kushagra",), React.createElement("h2",{},"Mishra")
+                ]),
+                React.createElement("div",{id: "child2"},
+                    [React.createElement("h1",{},"My Name is Kushagra",), React.createElement("h2",{},"Mishra")
+                ])]
+            );
+            console.log(parent);
+            const root = ReactDOM.createRoot(document.getElementById("root"));
+            root.render(parent);
