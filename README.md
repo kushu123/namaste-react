302e2a481745c3baa481e8314ae536ca2777c64a
Kushagra Mishra <kushagramishra@Kushagras-MacBook-Air.local>
2024-04-15 19:50:20 +0530

    Namaste React

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Namaste React🚀
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image optimization
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+      import React from "react";
+      import ReactDOM from "react-dom/client";      
+            
             const heading = React.createElement(
                 "h1", 
                 {id : "heading"}, 
@@ -8,12 +11,14 @@
 
             const parent = React.createElement("div",{id: "parent"},
                 [React.createElement("div",{id: "child1"},
-                    [React.createElement("h1",{},"My Name is Kushagra",), React.createElement("h2",{},"Mishra")
+                [React.createElement("h1",{},"My Name is Kushagra",), React.createElement("h2",{},"Mishra")
                 ]),
                 React.createElement("div",{id: "child2"},
-                    [React.createElement("h1",{},"My Name is Kushagra",), React.createElement("h2",{},"Mishra")
+                    [React.createElement("h1",{},"My Name is Kushagra",), 
+                    React.createElement("h2",{},"Mishra")
                 ])]
             );
             console.log(parent);
             const root = ReactDOM.createRoot(document.getElementById("root"));
             root.render(parent);
+            
